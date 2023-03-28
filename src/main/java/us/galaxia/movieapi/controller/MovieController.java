19d809c699bab7ab5c5235f3c8260d427a749773
WxAtwoo <Will.Atwood@mclaneco.com>
2023-03-28 10:44:49 -0400

    Adding FindAndModify method based on Movie title. https://medium.com/geekculture/types-of-update-operations-in-mongodb-using-spring-boot-11d5d4ce88cf

--- src/main/java/us/galaxia/movieapi/controller/MovieController.java
+++ src/main/java/us/galaxia/movieapi/controller/MovieController.java
@@ -4,14 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import us.galaxia.movieapi.models.MovieModel;
 import us.galaxia.movieapi.service.MovieService;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -37,8 +35,17 @@ public class MovieController {
         return new ResponseEntity<>(movieService.findMovieByImdbId(imdbId), HttpStatus.OK);
     }
 
+    @PostMapping("/title")
+    public ResponseEntity<MovieModel> updateMovieTitleByImdbId(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<>(movieService.updateMovieTitleByImdbId(payload.get("oldTitle"), payload.get("newTitle")), HttpStatus.CREATED);
+    }
+
     @GetMapping("/genre/{genre}")
     public ResponseEntity<Optional<List<MovieModel>>> getMoviesByGenre(@PathVariable String genre) {
         return new ResponseEntity<>(movieService.findMoviesByGenre(genre), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<MovieModel> createMovie(@RequestBody MovieModel movieModel) {
+        return new ResponseEntity<>(movieService.createMovie(movieModel), HttpStatus.CREATED);
     }
 }
--- src/main/java/us/galaxia/movieapi/service/MovieService.java
+++ src/main/java/us/galaxia/movieapi/service/MovieService.java
@@ -2,6 +2,11 @@ package us.galaxia.movieapi.service;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import us.galaxia.movieapi.repository.MovieRepository;
 import us.galaxia.movieapi.models.MovieModel;
@@ -14,6 +19,8 @@ public class MovieService {
 
     @Autowired
     private MovieRepository movieRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public List<MovieModel> getAllMovies() {
         return movieRepository.findAll();
@@ -29,5 +36,17 @@ public class MovieService {
 
     public Optional<List<MovieModel>> findMoviesByGenre(String genre) {
         return movieRepository.findMovieModelByGenres(genre);
+    }
+
+    public MovieModel createMovie(MovieModel movieModel) {
+        return movieRepository.insert(movieModel);
+    }
+
+    public MovieModel updateMovieTitleByImdbId(String oldTitle, String newTitle) {
+        Query query = new Query().addCriteria(Criteria.where("title").is(oldTitle));
+        Update updateDefinition = new Update().set("title", newTitle);
+        FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true).upsert(false);
+
+        return mongoTemplate.findAndModify(query, updateDefinition, options, MovieModel.class);
     }
 }
